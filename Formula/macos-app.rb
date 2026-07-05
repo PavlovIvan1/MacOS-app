0cf7c2a14dcef7532f4d0e0047df78702a20e251
Ivan Pavlov <ivanpavlovdeveloper@gmail.com>
2026-07-05 19:35:18 +0300

    Add Homebrew formula

---
+++ Formula/macos-app.rb
@@ -0,0 +1,18 @@
+class MacosApp < Formula
+  desc "Простое SwiftUI macOS-приложение (Hello, World! / Привет!)"
+  homepage "https://github.com/PavlovIvan1/MacOS-app"
+  url "https://github.com/PavlovIvan1/MacOS-app/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "4936b4b661edd1dcc95633bfbb603a2a3def39bd592f9971ddcf9aa1bcf234be"
+  license "MIT"
+
+  depends_on xcode: :build
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/HelloWorldApp" => "macos-app"
+  end
+
+  test do
+    assert_predicate bin/"macos-app", :exist?
+  end
+end
